f28d4c2049d3105e855123a06425b9e121d53e93
Monk Liu <Monk.Liu@amd.com>
2019-05-05 18:49:23 +0800

    add some hint
    Signed-off-by: Monk Liu <Monk.Liu@amd.com>

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+#please prepare gim-api sriov_drv and kalloc ready in drivers folder before building kernel
+#check drivers/Makefile for details
 fakeroot make-kpkg --initrd --append-to-version=$1 kernel_image kernel_headers -j `grep -c ^processor /proc/cpuinfo`
