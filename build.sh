e25fb33f59e5df6713c1c7705e14f06a0c6244dd
Monk Liu <Monk.Liu@amd.com>
2019-07-02 16:00:33 +0800

    use this build.sh to generate good header that allow GIM build
    Signed-off-by: Monk Liu <Monk.Liu@amd.com>

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #please prepare gim-api sriov_drv and kalloc ready in drivers folder before building kernel
 #check drivers/Makefile for details
-fakeroot make-kpkg --initrd --append-to-version=$1 kernel_image kernel_headers -j `grep -c ^processor /proc/cpuinfo`
+#fakeroot make-kpkg --initrd --append-to-version=$1 kernel_image kernel_headers -j `grep -c ^processor /proc/cpuinfo`
+fakeroot make -j 15 deb-pkg LOCALVERSION=$1 KDEB_PKGVERSION=$(make kernelversion)
